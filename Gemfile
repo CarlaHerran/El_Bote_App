31f1bc345f11fceb19797b631f80a5639126856c
Carla Herrán <carlaherran@MacBook-Pro-de-Carla.local>
2017-10-09 21:51:27 -0500

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -12,6 +12,10 @@ gem 'rails', '~> 5.1.4'
 # Use Puma as the app server
 gem 'bcrypt',         '3.1.11'
 
+gem 'faker',          '1.7.3'
+gem 'will_paginate',           '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
+
 # gem 'rails',          '5.1.2'
 gem 'bootstrap-sass', '3.3.7'
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,10 @@ module SessionsHelper
   end
 
 
-
+# Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
 
 
   # Returns the current logged-in user (if any).
@@ -54,6 +57,19 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 
